6b2f0fa2f3e25c64bb4560f5ad588bb744809998
Victor Tommasi <vtomm6575@gmail.com>
2018-08-06 18:08:02 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './cv/header/header.component';
+import { LandingComponent } from './cv/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
